4dd3133f3733c5f1b1895a83e77cf9e17d776354
Jason <jcubic@gmail.com>
2018-09-30 16:39:57 -0700

    pub issue

--- src/utilities/get-transition-duration-from-element.ts
+++ src/utilities/get-transition-duration-from-element.ts
@@ -5,18 +5,20 @@
 // import _toLower from 'lodash/toLower';
 
 
-const toNumber = require('lodash/toNumber');
-const max = require('lodash/max');
-const isNaN = require('lodash/isNaN');
-const trimEnd = require('lodash/trimEnd');
-const toLower = require('lodash/toLower');
-const split = require('lodash/split');
+// const toNumber = require('lodash/toNumber');
+// const max = require('lodash/max');
+// const isNaN = require('lodash/isNaN');
+// const trimEnd = require('lodash/trimEnd');
+// const toLower = require('lodash/toLower');
+// const split = require('lodash/split');
+
+import _ from 'lodash';
 
 function transformTransitionDuration(stringDuration) {
   // always get the first duration (same as bootstrap)
-  const durationArr = split(stringDuration, ',');
-  const timeInSeconds = toNumber(trimEnd(toLower(durationArr[0]), 's'));
-  if (isNaN(timeInSeconds)) {
+  const durationArr = _.split(stringDuration, ',');
+  const timeInSeconds = _.toNumber(_.trimEnd(_.toLower(durationArr[0]), 's'));
+  if (_.isNaN(timeInSeconds)) {
     return 0;
   }
   return timeInSeconds * 1000;
@@ -33,5 +35,5 @@ export default function getTransitionDurationFromElement(element) {
   const durationArr = [];
   durationArr.push(transformTransitionDuration(animationDuration));
   durationArr.push(transformTransitionDuration(transitionDuration));
-  return max(durationArr);
+  return _.max(durationArr);
 }
